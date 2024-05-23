a92ad48d3ad30a80d1fa7f5d70f859560516af2e
siso <116954896+sisology@users.noreply.github.com>
2024-05-23 13:13:50 +0900

    backSetting

--- src/main/java/yep/greenFire/greenfirebackend/GreenFireBackendApplication.java
+++ src/main/java/yep/greenFire/greenfirebackend/GreenFireBackendApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class GreenFireBackendApplication {
 
+
+
+
     public static void main(String[] args) {
         SpringApplication.run(GreenFireBackendApplication.class, args);
     }
