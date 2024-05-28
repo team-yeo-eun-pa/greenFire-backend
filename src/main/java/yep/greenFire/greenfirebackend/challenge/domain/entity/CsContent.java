b424ab4e71ef040e482b6458a20e7ec237480fa7
hohojeong091 <hj660833@gmail.com>
2024-05-28 11:03:49 +0900

    5/28 #15 회원 : 관리자 문의 게시판 개발중  : 1차 푸시 테스트중

--- src/main/java/yep/greenFire/greenfirebackend/challenge/domain/entity/CsContent.java
+++ src/main/java/yep/greenFire/greenfirebackend/challenge/domain/entity/CsContent.java
@@ -1,4 +1,9 @@
 package yep.greenFire.greenfirebackend.challenge.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tbl_cs")
 public class CsContent {
 }
