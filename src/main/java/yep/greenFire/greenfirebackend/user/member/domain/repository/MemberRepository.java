a92bb7840871bb619fb9f9d515715be29655e72e
jeong-gayeon <1bfish106@naver.com>
2024-05-29 21:23:17 +0900

    feat: Admin Report

--- src/main/java/yep/greenFire/greenfirebackend/user/member/domain/repository/MemberRepository.java
+++ src/main/java/yep/greenFire/greenfirebackend/user/member/domain/repository/MemberRepository.java
@@ -3,7 +3,6 @@ package yep.greenFire.greenfirebackend.user.member.domain.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import yep.greenFire.greenfirebackend.admin.member.domain.entity.AdminMember;
 import yep.greenFire.greenfirebackend.user.member.domain.entity.Member;
 import yep.greenFire.greenfirebackend.user.member.domain.type.MemberStatus;
 
