5548d4a32323dad0b9cfaa66d4e4a8147900aea4
siso <116954896+sisology@users.noreply.github.com>
2024-05-30 10:07:56 +0900

    Update AuthService.java

--- src/main/java/yep/greenFire/greenfirebackend/auth/service/AuthService.java
+++ src/main/java/yep/greenFire/greenfirebackend/auth/service/AuthService.java
@@ -77,8 +77,6 @@ public class AuthService implements UserDetailsService {
 
         CustomUser customUser = new CustomUser(loginDTO.getMemberCode(), user);
 
-        CustomUser customUser = new CustomUser(loginDTO.getMemberCode(), user);
-
         Authentication authentication
                 = new UsernamePasswordAuthenticationToken(customUser, null, customUser.getAuthorities());
         SecurityContextHolder.getContext().setAuthentication(authentication);
