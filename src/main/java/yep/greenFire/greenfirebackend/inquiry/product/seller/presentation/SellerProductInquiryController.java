b6734a8161034dc664c8f2ef5dd24ce234c5d680
hohojeong091 <hj660833@gmail.com>
2024-06-03 17:39:55 +0900

    feat: seller-product-inquiry-view

---
+++ src/main/java/yep/greenFire/greenfirebackend/inquiry/product/seller/presentation/SellerProductInquiryController.java
@@ -0,0 +1,48 @@
+package yep.greenFire.greenfirebackend.inquiry.product.seller.presentation;
+
+
+import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import yep.greenFire.greenfirebackend.common.paging.Pagination;
+import yep.greenFire.greenfirebackend.common.paging.PagingButtonInfo;
+import yep.greenFire.greenfirebackend.common.paging.PagingResponse;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.domain.repository.SellerProductInquiryRepository;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.dto.request.SellerProductReplyCreateRequest;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.dto.response.SellerProductInquiryResponse;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.service.SellerProductInquiryService;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/inquiry/product/seller")
+public class SellerProductInquiryController {
+
+    private SellerProductInquiryService sellerProductInquiryService;
+
+
+
+    @GetMapping("/list")
+    public ResponseEntity<PagingResponse> getProductInquiryList(
+            @RequestParam(defaultValue = "1") final Integer page
+    ) {
+        final Page<SellerProductInquiryResponse> sellerProductInquiryResponse = sellerProductInquiryService.getProductInquiryList(1L, page);
+        final PagingButtonInfo pagingButtonInfo = Pagination.getPagingButtonInfo(sellerProductInquiryResponse);
+        final PagingResponse pagingResponse = PagingResponse.of(sellerProductInquiryResponse.getContent(), pagingButtonInfo);
+
+        return ResponseEntity.ok(pagingResponse);
+    }
+
+    @PostMapping("/regist")
+    public ResponseEntity<SellerProductInquiryResponse> save (
+            @RequestPart @Valid SellerProductReplyCreateRequest sellerProductReplyCreateRequest
+            ) {
+        final int sellerProductReplyCode =  sellerProductInquiryService.save(sellerProductReplyCreateRequest);
+        return ResponseEntity.created(URI.create("inquiry/product/seller/list" + sellerProductReplyCode)).build();
+    }
+
+
+
+
+}
---
+++ src/main/java/yep/greenFire/greenfirebackend/inquiry/product/seller/service/SellerProductInquiryService.java
@@ -0,0 +1,47 @@
+package yep.greenFire.greenfirebackend.inquiry.product.seller.service;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import yep.greenFire.greenfirebackend.inquiry.entity.InquiryContent;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.domain.repository.SellerProductInquiryRepository;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.dto.request.SellerProductReplyCreateRequest;
+import yep.greenFire.greenfirebackend.inquiry.product.seller.dto.response.SellerProductInquiryResponse;
+
+@Service
+public class SellerProductInquiryService {
+
+    private SellerProductInquiryRepository sellerProductInquiryRepository;
+    private Pageable getPageable(final Integer page) {
+        return PageRequest.of(page - 1, 10, Sort.by("inquiryCode").descending());
+    }
+
+
+    public Page<SellerProductInquiryResponse> getProductInquiryList(long productCode, Integer page) {
+        Page<InquiryContent> sellerProductInquiry = sellerProductInquiryRepository.findByProductCode(productCode, getPageable(page));
+
+        return sellerProductInquiry.map(SellerProductInquiryResponse::from);
+
+    }
+
+    public int save(SellerProductReplyCreateRequest sellerProductReplyCreateRequest) {
+
+        final InquiryContent newProductInquiryReply = InquiryContent.of4(
+                sellerProductReplyCreateRequest.getProductCode(),
+                sellerProductReplyCreateRequest.getProductName(),
+                sellerProductReplyCreateRequest.getMemberCode(),
+                sellerProductReplyCreateRequest.getInquiryTitle(),
+                sellerProductReplyCreateRequest.getInquiryDetail(),
+                sellerProductReplyCreateRequest.getInquiryWriteDate(),
+                sellerProductReplyCreateRequest.getInquiryReply(),
+                sellerProductReplyCreateRequest.getInquiryReplyStatus()
+
+        );
+
+        final InquiryContent newProductReply = sellerProductInquiryRepository.save(newProductInquiryReply);
+        return newProductReply.getInquiryCode();
+
+    }
+}
