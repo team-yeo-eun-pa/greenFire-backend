0b8c442a04a13b1576c10d5e22b69b98c5432891
syyyl <lxxsxyxx@gmail.com>
2024-06-05 12:16:46 +0900

    #6 feat: request product option

--- src/main/java/yep/greenFire/greenfirebackend/auth/config/SecurityConfig.java
+++ src/main/java/yep/greenFire/greenfirebackend/auth/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                     auth.requestMatchers(HttpMethod.POST, "/members/signup", "/members/login").permitAll();
                     auth.requestMatchers("/admin/**").hasRole(MemberRole.ADMIN.toString());
                     auth.requestMatchers("/seller/**").hasRole(MemberRole.SELLER.toString());
+                    auth.requestMatchers(HttpMethod.GET,"/product/**").permitAll();
                     auth.requestMatchers(HttpMethod.GET,"/Member/notices/**").permitAll();
                     auth.anyRequest().authenticated();
 
---
+++ src/main/java/yep/greenFire/greenfirebackend/product/domain/repository/ProductRepository.java
@@ -0,0 +1,55 @@
+package yep.greenFire.greenfirebackend.product.domain.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import yep.greenFire.greenfirebackend.product.domain.entity.Product;
+import yep.greenFire.greenfirebackend.product.domain.type.SellableStatus;
+import yep.greenFire.greenfirebackend.product.dto.response.ProductResponse;
+import yep.greenFire.greenfirebackend.product.dto.response.ProductsResponse;
+
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    
+    @Query(
+            "select new yep.greenFire.greenfirebackend.product.dto.response.ProductsResponse(p, c, s) " +
+                    "from Product p join Category c on c.categoryCode = p.categoryCode " +
+                    "join Store s on s.storeCode = p.storeCode " +
+                    "where p.categoryCode = :categoryCode " +
+                    "and p.sellableStatus = :sellableStatus"
+    )
+    Page<ProductsResponse> findByCategoryCodeAndSellableStatus(Pageable pageable, Long categoryCode, SellableStatus sellableStatus);
+
+    @Query(
+            "select new yep.greenFire.greenfirebackend.product.dto.response.ProductsResponse(p, c, s) " +
+                    "from Product p join Category c on c.categoryCode = p.categoryCode " +
+                    "join Store s on s.storeCode = p.storeCode " +
+                    "where p.storeCode = :storeCode " +
+                    "and p.sellableStatus = :sellableStatus"
+    )
+    Page<ProductsResponse> findByStoreCodeAndSellableStatus(Pageable pageable, Long storeCode, SellableStatus sellableStatus);
+
+    @Query(
+            "select new yep.greenFire.greenfirebackend.product.dto.response.ProductsResponse(p, c, s) " +
+                    "from Product p join Category c on c.categoryCode = p.categoryCode " +
+                    "join Store s on s.storeCode = p.storeCode " +
+                    "where p.sellableStatus = :sellableStatus"
+    )
+    Page<ProductsResponse> findBySellableStatus(Pageable pageable, SellableStatus sellableStatus);
+
+    @Query(
+            "select new yep.greenFire.greenfirebackend.product.dto.response.ProductsResponse(p, c, s) " +
+                    "from Product p join Category c on c.categoryCode = p.categoryCode " +
+                    "join Store s on s.storeCode = p.storeCode " +
+                    "where p.productName like %:productName% " +
+                    "and p.sellableStatus = :sellableStatus"
+    )
+    Page<ProductsResponse> findByProductNameContainsAndSellableStatus(Pageable pageable, String productName, SellableStatus sellableStatus);
+
+
+}
