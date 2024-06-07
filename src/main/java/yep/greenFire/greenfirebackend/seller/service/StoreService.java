82566ff863564ebc81fb4aefce1e4a56425e622a
jeong-gayeon <1bfish106@naver.com>
2024-06-08 01:00:53 +0900

    [Refactor] Admin Report View #94 pull merge

--- src/main/java/yep/greenFire/greenfirebackend/seller/service/StoreService.java
+++ src/main/java/yep/greenFire/greenfirebackend/seller/service/StoreService.java
@@ -1,14 +1,17 @@
-package yep.greenFire.greenfirebackend.seller.service;
+//package yep.greenFire.greenfirebackend.seller.service;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//import yep.greenFire.greenfirebackend.store.domain.repository.StoreRepository;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class StoreService {
+//
+//    private final StoreRepository storeRepository;
+//
+//}
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import yep.greenFire.greenfirebackend.store.domain.repository.StoreRepository;
-
-@Service
-@RequiredArgsConstructor
-public class StoreService {
-
-    private final StoreRepository storeRepository;
-
-}
+// to 시온이
+// 시온아 이거 storeService랑 이름이 겹쳐서 bean등록이 안된대 그래서 주석해놨어..
 
