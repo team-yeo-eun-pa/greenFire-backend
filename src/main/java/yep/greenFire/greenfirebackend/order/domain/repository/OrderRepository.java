bc1ff60f038deb6cd48947cffd66072442ddf0a8
syyyl <lxxsxyxx@gmail.com>
2024-06-14 09:37:51 +0900

    #74 fix: revert files

--- src/main/java/yep/greenFire/greenfirebackend/order/domain/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package yep.greenFire.greenfirebackend.order.domain.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import yep.greenFire.greenfirebackend.order.domain.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long>, OrderRepositoryCustom {
-
-}
-
